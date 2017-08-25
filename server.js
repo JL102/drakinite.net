777b8be1e04564a102a91177a83c315ab134672d
ASUS\Drak <jordannamez@gmail.com>
2017-08-25 00:51:48 -0400

    added some super basic logging stuff

--- server.js
+++ server.js
@@ -19,12 +19,18 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users\
+
+app.use(function (req, res, next) {
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	console.log('Request at ', new Date(), ' from ', ip);
+	next()
+}) //Logs a request
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
 
 // start app ===============================================
-app.listen(port);	
+app.listen(port);	//Listen on port
 console.log('Magic happens on port ' + port); 			// shoutout to the user
 exports = module.exports = app; 						// expose app
