e39d22add9fe5349ddfb2caa0546e9ee61041752
JL102 <jordannamez@gmail.com>
2018-08-26 23:47:10 -0400

    fix

--- useFunctions.js
+++ useFunctions.js
@@ -2,11 +2,17 @@ var functions = {};
 
 functions.drakRedirect = require('express').Router();
 functions.drakRedirect.get('/*', function(req, res){
-  res.redirect("https://drakinite.net" + req.url);
+  var isSSL = (req.socket.encrypted ? true : false);
+  if(!isSSL){
+    res.redirect("https://drakinite.net" + req.url);
+  }
 })
 functions.jordanRedirect = require('express').Router();
 functions.jordanRedirect.get('/*', function(req, res){
-  res.redirect("https://jordanle.es" + req.url);
+  var isSSL = (req.socket.encrypted ? true : false);
+  if(!isSSL){
+    res.redirect("https://jordanle.es" + req.url);
+  }
 })
 
 //shared logger function
