0d33ecaa8a455770a4c33dfb90000f53941515da
JL102 <jordannamez@gmail.com>
2020-12-01 21:02:03 -0500

    dumb counter

--- routes/drak/index.js
+++ routes/drak/index.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var LDC = require('../../scripts/lupusDumbCounter');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('./drak/index', { title: 'Welcome' });
+router.get('/', async (req, res, next) => {
+	res.render('./drak/index', { title: 'Welcome' });
 });
 
-router.get('/badge', async function (req, res, next) {
-  res.redirect('/badges');
+router.get('/badge', async (req, res, next) => {
+	res.redirect('/badges');
+});
+
+router.get('/badges*', async (req, res, next) => {
+	res.render('./drak/badges', { title: 'Get a Custom Sona Badge!' });
+});
+
+router.get('/lupusdumbcounter', async (req, res, next) => {
+	
+	var dumbThingLabel = req.query.dumbThingLabel || 'Wrong Layers';
+	
+	res.render('./drak/lupusdumbcounter', {
+		label: dumbThingLabel,
+		count: LDC.getValue(),
+	});
+});
+
+router.get('/lupusdumbcounter/addpoint', async (req, res, next) => {
+	LDC.add();
+	res.status(200).send();
 })
 
-router.get('/badges*', async function (req, res, next) {
-  res.render('./drak/badges', { title: 'Get a Custom Sona Badge!' });
-})
 
 module.exports = router;
---
+++ scripts/lupusDumbCounter.js
@@ -0,0 +1,28 @@
+var LDC = {
+	init: function() {
+		this.sockets = [];
+		this.value = 0;
+	},
+	addWebsocket: function(socket) {
+		this.sockets.push(socket);
+	},
+	add: function() {
+		this.value += 1;
+		this.sendMessage(this.value);
+	},
+	clear: function() {
+		this.value = 0;
+		this.sendMessage(this.value);
+	},
+	sendMessage: function(message) {
+		for (var socket of this.sockets) {
+			socket.send(message);
+		}
+	},
+	getValue: function() {
+		return this.value;
+	}
+};
+
+LDC.init();
+module.exports = LDC;
